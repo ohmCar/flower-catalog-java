d81ad43c4ee0821f5299b608f54fd252859bb3c3
omkar mote <omkarmote210@gmail.com>
2018-03-29 21:26:36 +0530

    Initial commit.

---
+++ src/main/java/com/tw/servlet/Hello.java
@@ -0,0 +1,14 @@
+package com.tw.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Hello extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.getWriter().write("<b> Hello omkar!</b>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+  <head>
+    <title>Flower Catalog</title>
+    <link rel="stylesheet" href="./css/style.css">
+  </head>
+  <body>
+  <h2> Flower Catalog </h2>
+  <hr>
+  <img src="images/flower.jpg"/>
+  </body>
+</html>
